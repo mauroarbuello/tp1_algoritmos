540d547cecce8c74170cafc70267793b07c6ecf6
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-13 11:43:31 -0300

    Actualizacion del fix_t ¿version definitiva?
    Agregado el tipo calidad_t con valores del 0 al 8
    Se agregó al fix_t los datos que faltaban.

--- main.c
+++ main.c
@@ -5,24 +5,31 @@
 bool procesar_argv(int argc, char *argv[], struct fix_t *fix);
 void get_fechaactual(struct fix_t *fix);
 
-typedef struct fecha{int year;
-					int month;
-					int day;
-					}fecha_t
+typedef struct fecha{	int year;	//estructura para la fecha
+			int month;
+			int day;
+			}fecha_t;
 
-typedef struct hora{int hh;
-					int mm;
-					double ss;
-					}hora_t
+typedef struct hora{	int hh;		//estructura para la hora
+			int mm;
+			double ss;
+			}hora_t;
 
-typedef struct fix{char nombre[MAX_NOMBRE];
-					struct fecha_t fecha;
-					struct hora_t hora;
-					double latitud;
-					double longitud;
-					/* Aca hay que completar los campos necesarios */			
+typedef enum calidad{ 0; 1; 2; 3; 4; 5; 6; 7; 8; }calidad_t;	//tipo para la calidad del fix
 					
-					}fix_t;
+				
+
+typedef struct fix{	char nombre[MAX_NOMBRE];	//estructura con todos los datos del fix
+			struct fecha_t fecha;
+			struct hora_t hora;
+			double latitud;
+			double longitud;
+			calidad_t calidad;
+			unsigned int cant_satelites;
+			float hdop;
+			float elevacion;
+			float sep_geoide;					
+			}fix_t;
 
 					
 int main(int argc, char* argv[]){
