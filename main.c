0cf7e50486830e740bfb75bb422e52ada06b72e0
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-14 02:32:30 -0300

    Cambios en el uso de get_fechactual
    Cambios en los argumentos ya que cambió la función

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include <fix.h>
 
 bool procesar_argv(int argc, char *argv[], struct fix_t *fix);
-void get_fechaactual(struct fix_t *fix);
+void get_fechaactual(struct fecha_t *fecha);
 					
 int main(int argc, char* argv[]){
 	
 	struct fix_t fixactual;
 	
-	get_fechaactual(&fixactual);
+	get_fechaactual(&(fixactual.fecha));
 	
 	procesar_argv(argc,argv,&fixactual);
 	
