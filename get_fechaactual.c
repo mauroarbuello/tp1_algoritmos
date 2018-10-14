83a10c1c3523914e9c29408c87814c5a7acb063d
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-14 01:50:50 -0300

    Cambie la funcion get_fechaactual
    La cambie para que pida por parametro un tipo struct fecha_t

--- get_fechaactual.c
+++ get_fechaactual.c
@@ -1,12 +1,12 @@
 #include <time.h>
 #include <fix.h>
 
-void get_fechaactual(struct fix_t *fix){
+void get_fechaactual(struct fecha_t *fecha){
 	
-	struct tm* tiempo= *localtime(time_t(NULL));
+	struct tm tiempo= *localtime(time_t(NULL));
 	
-	fix->fecha.year = tiempo.tm_year;
-	fix->fecha.month = tiempo.tm_month;
-	fix->fecha.day = tiempo.tm_day;
+	fecha->year = tiempo.tm_year;
+	fecha->month = tiempo.tm_month;
+	fecha->day = tiempo.tm_day;
 	
 }
