0fc866e32ead167a303bacb1edada6d19296a941
patovermal <44077261+patovermal@users.noreply.github.com>
2018-10-15 14:14:26 -0300

    Update chk_gga.c

--- chk_gga.c
+++ chk_gga.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-
-#define MAX_4 4
-
-#define MAX_STR 5000
 #define STR_GGA "GGA"
-//#define CHAR_PROC ,
 
+typedef enum {ST_OK, ST_ERR_DATO_INCOMP} status_t;
 
-int main (void) {
-  int val_chkgga; //devolver datos
-  int i, j; //contaores
+//
+status_t chk_gga (const char *cadena, char *salida_chkgga) {
+
+  int i, j;//contadores
   char * ptr2coma;
-  
-  const char cadena [50] = "$GPGA,$A ,3 ,01";
   const char * coma = ",";
-  char chk_gga [MAX_4];//inicializar // para chequear que la entrada sea GGA
+  int val_chkgga;
   size_t max_cmp4 = 4;
-  
-  ptr2coma = strstr (cadena, coma);
-  
-printf ("%s\n", ptr2coma); //HC
-  /*hay que validar el caso en que hay una ',' antes de la
-  tercera posicion*/
-  for (i = 0, j = 3; j > 0 && i < 3; i++, j--) {
-    chk_gga [i] = *(ptr2coma - j);
-    }// guarda los 3 char para luego comparar con GGA.
 
-  val_chkgga = strncmp (chk_gga, STR_GGA, max_cmp4);
- 
-    if (val_chkgga != 0) { // si el dato no es GGA
-    printf ("%s : %s\n", "error", chk_gga); // -------PRUEBA
-  } else {
-    printf ("%s : %s\n", chk_gga, "joya"); //-------- PRUEBAS
- 
+  ptr2coma = strstr (cadena, coma);
+
+  for (i = 0, j = 3; j > 0; i++, j--) {
+    salida_chkgga[i] = *(ptr2coma - j);
   }
-} //end main
+
+  val_chkgga = strncmp (salida_chkgga, STR_GGA, max_cmp4);
+
+  if (val_chkgga != 0) { // la cadena no decia GGA
+    return ST_ERR_DATO_INCOMP;
+  }
+  return ST_OK;
+}
+// end chk_gga
+//pruebas
+int main (void) {
+  const char cadena [50] = "$GPGGA,$A ,3 ,01";
+  char salida[4]; //HC
+
+  chk_gga (cadena, &salida);
+
+  printf("%s\n", salida);
+
+}
