a74573acebcfaef841c7dbe96784f60e0371c0b0
patovermal <44077261+patovermal@users.noreply.github.com>
2018-10-15 18:37:58 -0300

    Se deja de usar strlen y se busca un '\0' en for.

--- buscar_comas.c
+++ buscar_comas.c
@@ -8,9 +8,8 @@ status_t search_coma (char * cadena, char ** ptr2ptrarray) {
 	int j = 0;
 	int i;
 	const char coma = ',';
-	int largo = strlen(cadena);
 
-	for (i = 0; i<largo ; i++) {
+	for (i = 0; cadena[i] != '\0' ; i++) {
 		if ( cadena[i] == coma) {
 			*(ptr2ptrarray+j) = &(cadena[i]);
 			j++;
