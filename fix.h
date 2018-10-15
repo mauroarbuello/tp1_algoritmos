ec1045a6a9520977bb3fc92afd359bbdacd0b4f0
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-15 00:38:49 -0300

    Update fix.h
    Agregué un par de defines y comenté los diccionarios 
    porque me tiró error cuando intenté compilar
    con diccionarios

--- fix.h
+++ fix.h
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 
+#define MAX_NOMBRE			50
+#define MAX_STR 			100
+
 #define MSJ_ERR_PREFIJO 	"ERROR"
 
 #define TXT_CAL_INVALIDO 	"Fix Inválido"
@@ -12,35 +15,36 @@
 #define TXT_CAL_MANUAL 		"Fix Manual"
 #define TXT_CAL_SIMULATION	"Fix Simulación"
 
-#define TXT_ST_OK			"Correcto"
+#define TXT_ST_OK					"Correcto"
 #define TXT_ST_ERR_DATO_INCOMP		"Dato incompleto"
 #define TXT_ST_ERR_PUNT_NULL		"Puntero nulo"
 #define TXT_ST_ERR_FECHA_INVALIDA 	"Fecha ingresa invalida. Se utilizará la fecha del sistema."
 
-typedef enum status{ ST_OK, ST_ERR_DATO_INCOMP, ST_ERR_PUNT_NULL, ST_ERR_FECHA_INVALIDA}status_t //estados posibles dentro de todo el proyecto, hay que ir agregando
+typedef enum status_t{ ST_OK, ST_ERR_DATO_INCOMP, ST_ERR_PUNT_NULL, ST_ERR_FECHA_INVALIDA}status_t; //estados posibles dentro de todo el proyecto, hay que ir agregando
 
-const char dic_status[][MAX_STR]{
-	TXT_ST_OK, TXT_ST_ERR_DATO_INCOMP, TXT_ST_ERR_PUNT_NULL, TXT_ST_ERR_FECHA_INVALIDA}
+/* const char dic_status[][MAX_STR] = {
+	TXT_ST_OK, TXT_ST_ERR_DATO_INCOMP, TXT_ST_ERR_PUNT_NULL, TXT_ST_ERR_FECHA_INVALIDA}; */
 
-typedef struct fecha{	int year;	//estructura para la fecha
+typedef struct fecha_t{	int year;	//estructura para la fecha
 			int month;
 			int day;
 			}fecha_t;
 
-typedef struct hora{	int hh;		//estructura para la hora
+typedef struct hora_t{	int hh;		//estructura para la hora
 			int mm;
 			double ss;
 			}hora_t;
 
-typedef enum calidad{ 0, 1, 2, 3, 4, 5, 6, 7, 8, }calidad_t;	//tipo para la calidad del fix
+typedef enum calidad_t{ CAL_INVALIDO, CAL_FIX_GPS, CAL_FIX_DGPS, CAL_FIX_PPS, CAL_RTK, CAL_RTK_FLOAT, CAL_DEAD_RECK, CAL_MANUAL, CAL_SIMULATION }calidad_t;	//tipo para la calidad del fix
 
-const char dic_calidad [][MAX_STR] = {
+
+/* const char dic_calidad [][MAX_STR] = {
 	TXT_CAL_INVALIDO, TXT_CAL_FIX_GPS, TXT_CAL_FIX_DGPS, TXT_CAL_FIX_PPS, TXT_CAL_RTK, 
 	TXT_CAL_RTK_FLOAT, TXT_CAL_DEAD_RECK, TXT_CAL_MANUAL, TXT_CAL_SIMULATION
-};	//diccionario para la calidad
+};	//diccionario para la calidad */
 	
 
-typedef struct fix{	char nombre[MAX_NOMBRE];	//estructura con todos los datos del fix
+typedef struct fix_t{	char nombre[MAX_NOMBRE];	//estructura con todos los datos del fix
 			struct fecha_t fecha;
 			struct hora_t hora;
 			double latitud;
