1fcbe313aa796ec3eda6720ffa00afa592ef1d00
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-15 17:26:54 -0300

    Update get_fechaactual.c
    Actualizacion de get_fechaactual,
    Ahora recibe tambien una struct hora_t*
    Le pasa la fecha y ahora actual a las struct recibidas

--- get_fechaactual.c
+++ get_fechaactual.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include "fix.h"
 
-void get_fechaactual(struct fecha_t* fecha){
+void get_fechaactual(struct fecha_t* fecha, struct hora_t* hora){
 	
 	struct tm *tiempo;
 	time_t tiempo_seg;
 	tiempo_seg = time(NULL);
 	tiempo = localtime(&tiempo_seg);
 	
-	(*fecha).year = (tiempo->tm_year) +1900;
-	(*fecha).month = (tiempo->tm_mon) +1;
-	(*fecha).day = tiempo->tm_mday;
+	fecha->year 	= (tiempo->tm_year) +1900;
+	fecha->month 	= (tiempo->tm_mon) +1;
+	fecha->day 		= tiempo->tm_mday;
+	hora->hh		= tiempo->tm_hour;
+	hora->mm		= tiempo->tm_min;
+	hora->ss		= tiempo->tm_sec;
 	
 }
