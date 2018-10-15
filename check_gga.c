b1c90876ce773f191aa2bd7576cd1df63fca0368
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-15 18:26:16 -0300

    Update check_gga.c
    Actualizado con la funcion en base de la de pato
    Pero ahora es bool

--- check_gga.c
+++ check_gga.c
@@ -1,32 +1,25 @@
-#include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdbool.h>
 #include "fix.h"
 
 #define STR_GGA "GGA"
+#define MAX_CMP4 4
 
-bool chk_gga (const char * cadena, char** ptr2coma) {
+bool chk_gga (const char *cadena) {
 
-	size_t i, j;						//contadores
-	char cadena_aux[MAX_STR];			//cadena auxiliar
-    
-	for( i=0, j=0 ; *ptr2coma != &(cadena[i]) ; i++){ //itero hasta la primer coma de cadena
-		if ( isalpha(cadena[i]) ){					//si es alphanumerico el caracter, lo copio
-			cadena_aux[j] = cadena[i];
-			j++;
-		}
-	}
-	
-	cadena_aux[j] = '\0';			//le pongo un '\0' para finalizar la cadena
-	
-	if ( !strcmp(cadena_aux+2,STR_GGA) ){ //comparo los caracteres a partir del 3ro para ver si son GGA
-		
-		return true;
-	}
-	
-	return false;
-	
+  int i, j;//contadores
+  char salida_chkgga[MAX_CMP4];
+  char * ptr2coma;
+  const char * coma = ",";
+
+  ptr2coma = strstr (cadena, coma);
+
+  for (i = 0, j = 3; j > 0; i++, j--) {
+    salida_chkgga[i] = *(ptr2coma - j);
+  }
+
+  if ( strncmp (salida_chkgga, STR_GGA, MAX_CMP4) != 0) { // la cadena no decia GGA
+    return false;
+  }
+  return true;
 }
-			
-			
