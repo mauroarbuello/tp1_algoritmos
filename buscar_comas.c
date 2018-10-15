31db94f17fa343ae9985f5cc39e8d90999f3b542
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-15 01:49:07 -0300

    Update search_comas.c
    Ahora logré que funcione (hay que seguir comprobando)

--- buscar_comas.c
+++ buscar_comas.c
@@ -47,39 +47,36 @@ char ** search_coma ( const char * cadena) {
 }
 */
 
-status_t search_coma ( const char * cadena, char * (*ptr2ptrarray)[MAX_STR]) {
-  int j = 0;
-  int i;
-  const char coma = ',';
-  char * ptr2coma [MAX_STR];
+status_t search_coma ( char * cadena, char ** ptr2ptrarray) {
+	int j = 0;
+	int i;
+	const char coma = ',';
+	int largo = strlen(cadena);
 
-  for (i = 0; cadena[i] != '\0'; i++) {
-    if ( cadena[i] == coma) {
+	for (i = 0; i<largo ; i++) {
+		if ( cadena[i] == coma) {
+			*(ptr2ptrarray+j) = &(cadena[i]);
+			j++;
+		}
+	}
 
-    *(ptr2coma + j) = (cadena [i]);
-    j++;
-
-    }
-  }
-  ptr2ptrarray = &ptr2coma;
-  return ST_OK;
+	return ST_OK;
 }
 
 
 // pruebas para ver los valores que devuelve la funcion
 int main (void) {
-  char * (*arr_ptr2) [MAX_STR];
+  char * ptr2ptr[90];
   //char * ptr[MAX_STR];
-  const char cadena [90] = "$GPGGA ,112836.854 ,1955.118 , N1";
-  search_coma (cadena, &arr_ptr2);
+  char cadena [90] = "$GPGGA ,112836.854 ,1955.118 , N1";
+  search_coma (cadena, ptr2ptr);
   //printf ("|%c|\n", arr_ptr2);
   //printf ("|%p|\n", arr_ptr2);
-  printf ("valor arr_ptr2:      |%c|\n",     *(arr_ptr2));
-  printf ("direccion arr_ptr2:  |%p|\n",  arr_ptr2);
-  printf ("valor cadena:        |%c|\n",       *(cadena + 7));
-  printf ("direccion cadena:    |%p|\n",    (cadena + 7));
+  printf ("valor arr_ptr2:      |%c|\n",**ptr2ptr);
+  //printf ("direccion arr_ptr2:  |%p|\n",ptr2ptr);
+  printf ("valor cadena:        |%c|\n",*(cadena + 7));
+  printf ("direccion cadena:    |%p|\n",(cadena + 7));
   //printf ("|%c|\n", *(cadena + 7));
   //printf ("%p\n", (cadena + 7));
   //printf ("%p\n", *(cadena + 7));
 }
-
