d921d46c32c8d15f44821a86189b5d32993c07b9
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-15 00:37:40 -0300

    Logré que ande bien getfechaactual
    Logré que ande bien, no tira core dump

--- get_fechaactual.c
+++ get_fechaactual.c
@@ -1,12 +1,16 @@
 #include <time.h>
-#include <fix.h>
+#include <stdio.h>
+#include "fix.h"
 
-void get_fechaactual(struct fecha_t *fecha){
+void get_fechaactual(struct fecha_t* fecha){
 	
-	struct tm tiempo= *localtime(time_t(NULL));
+	struct tm *tiempo;
+	time_t tiempo_seg;
+	tiempo_seg = time(NULL);
+	tiempo = localtime(&tiempo_seg);
 	
-	fecha->year = tiempo.tm_year;
-	fecha->month = tiempo.tm_month;
-	fecha->day = tiempo.tm_day;
+	(*fecha).year = (tiempo->tm_year) +1900;
+	(*fecha).month = (tiempo->tm_mon) +1;
+	(*fecha).day = tiempo->tm_mday;
 	
 }
