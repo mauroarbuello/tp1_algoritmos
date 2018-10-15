43337d4b9f0a2b4c4583f1aedc2751cd7060e488
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-15 17:25:04 -0300

    Update buscar_comas.c
    Actualizacion de la funcion funcionando bien

--- buscar_comas.c
+++ buscar_comas.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define MAX_STR 50
-
-typedef enum {ST_OK = 0, ST_ERR_NULLPTR} status_t;
+#include "fix.h"
 
 status_t search_coma (char * cadena, char ** ptr2ptrarray) {
 	int j = 0;
@@ -22,13 +21,3 @@ status_t search_coma (char * cadena, char ** ptr2ptrarray) {
 
 	return ST_OK;
 }
-
-// pruebas para ver los valores que devuelve la funcion
-/* int main (void) {
-	
-	char *array2comas[90];
-	char cadena [90] = "$GPGGA ,112836.854 ,1955.118 , N1";
-	search_coma(cadena, array2comas);
-	
-
-} */
