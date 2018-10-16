1a616263e059968b4183b56842ba990b734be329
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-16 11:26:10 -0300

    Le agregué la proteccion de la multiple inclusion
    Agregada la proteccion, comentado los diccionarios
    Porque ahora estan en errores.c

--- fix.h
+++ fix.h
@@ -1,29 +1,21 @@
+#ifndef FIX_H		//proteccion a la inclusion multiple
+#define FIX_H 
+
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_NOMBRE			50
 #define MAX_STR 			100
+#define MAX_CALIDAD			8
+#define MAX_SATELITES		12
 
 #define MSJ_ERR_PREFIJO 	"ERROR"
 
-#define TXT_CAL_INVALIDO 	"Fix Inválido"
-#define TXT_CAL_FIX_GPS 	"Fix GPS"
-#define TXT_CAL_FIX_DGPS 	"Fix DGPS"
-#define TXT_CAL_FIX_PPS 	"Fix PPS"
-#define TXT_CAL_RTK 		"Fix Real Time Kinematic"
-#define TXT_CAL_RTK_FLOAT 	"Fix Float Real Time Kinematic"
-#define TXT_CAL_DEAD_RECK	"Fix Estimado"
-#define TXT_CAL_MANUAL 		"Fix Manual"
-#define TXT_CAL_SIMULATION	"Fix Simulación"
 
-#define TXT_ST_OK					"Correcto"
-#define TXT_ST_ERR_DATO_INCOMP		"Dato incompleto"
-#define TXT_ST_ERR_PUNT_NULL		"Puntero nulo"
-#define TXT_ST_ERR_FECHA_INVALIDA 	"Fecha ingresa invalida. Se utilizará la fecha del sistema."
-
-typedef enum status_t{ ST_OK, ST_ERR_DATO_INCOMP, ST_ERR_PUNT_NULL, ST_ERR_FECHA_INVALIDA}status_t; //estados posibles dentro de todo el proyecto, hay que ir agregando
+typedef enum status_t{ ST_OK, ST_ERR_DATO_INCOMP, ST_ERR_PUNT_NULL, ST_ERR_FECHA_INVALIDA, ST_ERR_FIX_INVALIDO, ST_ERR_MISSING_ARG}status_t; //estados posibles dentro de todo el proyecto, hay que ir agregando
 
 /* const char dic_status[][MAX_STR] = {
-	TXT_ST_OK, TXT_ST_ERR_DATO_INCOMP, TXT_ST_ERR_PUNT_NULL, TXT_ST_ERR_FECHA_INVALIDA}; */
+	TXT_ST_OK, TXT_ST_ERR_DATO_INCOMP, TXT_ST_ERR_PUNT_NULL, TXT_ST_ERR_FECHA_INVALIDA, TXT_ST_ERR_FIX_INVALIDO}; */
 
 typedef struct fecha_t{	int year;	//estructura para la fecha
 			int month;
@@ -38,11 +30,11 @@ typedef struct hora_t{	int hh;		//estructura para la hora
 typedef enum calidad_t{ CAL_INVALIDO, CAL_FIX_GPS, CAL_FIX_DGPS, CAL_FIX_PPS, CAL_RTK, CAL_RTK_FLOAT, CAL_DEAD_RECK, CAL_MANUAL, CAL_SIMULATION }calidad_t;	//tipo para la calidad del fix
 
 
-/* const char dic_calidad [][MAX_STR] = {
+/* const char dic_calidad [] [MAX_STR] = {
 	TXT_CAL_INVALIDO, TXT_CAL_FIX_GPS, TXT_CAL_FIX_DGPS, TXT_CAL_FIX_PPS, TXT_CAL_RTK, 
 	TXT_CAL_RTK_FLOAT, TXT_CAL_DEAD_RECK, TXT_CAL_MANUAL, TXT_CAL_SIMULATION
 };	//diccionario para la calidad */
-	
+		
 
 typedef struct fix_t{	char nombre[MAX_NOMBRE];	//estructura con todos los datos del fix
 			struct fecha_t fecha;
@@ -55,3 +47,5 @@ typedef struct fix_t{	char nombre[MAX_NOMBRE];	//estructura con todos los datos 
 			float elevacion;
 			float sep_geoide;					
 			}fix_t;
+			
+#endif
