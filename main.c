cf9bc58bb4d4575ee140874c0e5b162570b82ae9
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-10-17 09:29:13 -0300

    Update main.c
    Ahora le puse lo que habiamos probado
    en prueba.c, con algunos cambios
    y comentado linea a linea

--- main.c
+++ main.c
@@ -1,17 +1,60 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <fix.h>
+#include <string.h>
+#include "fix.h"
 
-bool procesar_argv(int argc, char *argv[], struct fix_t *fix);
-void get_fechaactual(struct fecha_t *fecha);
+#define NOMBRE_POR_DEFECTO		"archivo"
+#define CHAR_STR_START			'$'
+
+//funciones para argv
+void get_fechaactual(struct fecha_t *fecha, struct hora_t * hora);
+void procesar_argv(int argc, char *argv[], struct fix_t *fix);
+//funciones para la cadena nmea (fix)
+bool chk_gga (const char *cadena);
+bool verify_checksum(const char* str_origen);
+status_t search_coma (char * cadena, char ** ptr2ptrarray);
+status_t procesar_fix(char ** ptr2coma,struct fix_t * fix);
+//funciones de impresion
+void print_encabezado(void);
+void print_metadata(struct fix_t * fix);
+void print_trk_start(void);
+void print_trkpt(struct fix_t* fix);
+void print_trk_end(void);
+void print_acabado(void);
 					
 int main(int argc, char* argv[]){
 	
 	struct fix_t fixactual;
+	char str_actual[MAX_STR];
+	char *array2comas[MAX_STR];
 	
-	get_fechaactual(&(fixactual.fecha));
+	strcpy(fixactual.nombre,NOMBRE_POR_DEFECTO);	//le copio el nombre por defecto a la struct fix_t
 	
-	procesar_argv(argc,argv,&fixactual);
+	get_fechaactual(&(fixactual.fecha),&(fixactual.hora));	//obtengo la fecha y hora actual y la paso al fix_t
 	
+	procesar_argv(argc,argv,&fixactual);	//proceso los argumentos por linea de comandos, si hay nombre o fecha los piso con los que tengo
+	
+	print_encabezado();	//imprimo el encabezado del archivo .gpx
+	print_metadata(&fixactual);	//imprimo la metadata (nombre, fecha, un par de cosas más)
+	print_trk_start();	//imprimo el comienzo de un track
+	
+	while( fgets(str_actual,MAX_STR,stdin) != NULL ){	//leo la cadena, cuando fgets me devuelva NULL quiere decir que llegó a EOF
+		if ( *(str_actual) == CHAR_STR_START ){		//compruebo que el primer caracter de la cadena sea $
+			if ( chk_gga(str_actual) ){			//compruebo que corresponde a un GGA
+				if ( verify_checksum(str_actual) ){		//hago la suma de verificacion
+					search_coma(str_actual,array2comas);		//genero un array de punteros a comas
+					if ( procesar_fix(array2comas,&fixactual) == ST_OK )	//proceso la cadena
+						print_trkpt(&fixactual);	//si dio todo OK la imprimo
+				}
+			}
+		}
+	}
+	//si sale es porque llegó al EOF
+	
+	print_trk_end();		//imprimo la finalizacion de un track
+	print_acabado();		// imprimo la ultima parte del archivo .gpx
+	
+	return EXIT_SUCCESS;		//devuelvo EXIT_SUCCESS, el programa se ejecutó bien
+
 }
